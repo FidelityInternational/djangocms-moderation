61e856b69b4e103a5fe1b72d6197761d17fd2a21
Damilare Onajole <web.dami@gmail.com>
2018-07-18 16:48:47 +0100

    Rework app config validation logic

--- djangocms_moderation/cms_config.py
+++ djangocms_moderation/cms_config.py
@@ -1,26 +1,26 @@
-from collections import Iterable
 from django.core.exceptions import ImproperlyConfigured
 from cms.app_base import CMSAppExtension
-
-from djangocms_versioning.models import BaseVersion
 
 
 class ModerationExtension(CMSAppExtension):
 
     def __init__(self):
-        self.versioned_moderation_models = []
+        self.moderated_models = []
 
     def configure_app(self, cms_config):
-        versioned_models = getattr(cms_config, 'versioned_moderation_models', [])
+        moderation_enabled = getattr(cms_config, 'moderation_enabled', False)
+        versioning_enabled = getattr(cms_config, 'django_versioning_enabled', False)
+        moderated_models = getattr(cms_config, 'moderated_models', [])
+        versioning_models = getattr(cms_config, 'versioning_models', [])
 
-        if isinstance(versioned_models, Iterable):
-            for versioned_model in versioned_models:
-                if issubclass(versioned_model, BaseVersion):
-                    self.versioned_moderation_models.append(versioned_model)
-                else:
-                    raise ImproperlyConfigured(
-                "Moderation model must be versionable, it should extend djangocms_versioning.models.BaseVersion")
+        if moderation_enabled:
+            if not versioning_enabled:
+                raise ImproperlyConfigured('Versioning needs to be enabled for Moderation')
+
+        for moderated_model in moderated_models:
+            if moderated_model not in versioning_models:
+                raise ImproperlyConfigured('Moderated models need to be Versionable, please include every \
+                model that needs to be moderated in ythe versioning_models entry')
         
-        else:
-            raise ImproperlyConfigured(
-                "versioned_moderation_models must be a Iterable object")
+
+        
